a3699be573a3ee7eb03cf23a6d39c4529f134db3
unknown <alisonvicab75@gmail.com>
2018-10-12 10:24:53 -0500

    Tarea

---
+++ TareaClase2/src/tareaclase2/Precio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase2;
+
+/**
+ *
+ * @author Victoria
+ */
+public class Precio {
+
+    double euros;
+
+    public double obtener() {
+        return 0;
+        //too
+    }
+
+    public void agregar(double x) {
+
+    }
+
+}
---
+++ TareaClase2/src/tareaclase2/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase2;
+
+/**
+ *
+ * @author Victoria
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Precio p;
+        p = new Precio();
+        System.out.println(p.euros);
+    }
+
+}
